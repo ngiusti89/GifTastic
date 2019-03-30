f2d3c1c7c52d414d547b06a6ea40ac00f9555ea6
Natasha Giusti <natashagiusti@natashas-mbp.lan>
2019-03-30 02:22:44 -0700

    buttons display gif stills

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,6 +1,5 @@
-$(document).ready(function() {
-    // document ready
-    console.log("ready");
+$(document).ready(function () {
+    console.log("ready"); // document ready
 
     // array of 90s movies called topics
     var topics = ["Jurassic Park", "Titanic", "The Lion King", "Space Jam", "10 Things I Hate About You", "Home Alone", "The Matrix"];
@@ -19,7 +18,7 @@ $(document).ready(function() {
             // adds display text (movie names) to button from array
             btn.text(topics[i]);
             // adds all the buttons to one class called 90s-movies
-            btn.addClass('90s-movies');
+            btn.addClass('movies');
             // puts buttons in the div and appends them after one another
             $('#buttons-div').append(btn);
         }
@@ -28,27 +27,40 @@ $(document).ready(function() {
     createButtons();
 
 
-    $('.90s-movies').click(function() {
+    $('.movies').click(function () {
         console.log($(this).data('name')); // test buttons pull movie names
-
         var movie = $(this).attr("data-name");
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + movie + "&api_key=r5rzmEF5dvBlgMheFWcsLR869s2tvSwr";
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + movie + "&api_key=r5rzmEF5dvBlgMheFWcsLR869s2tvSwr&limit=10";
 
-$.ajax({
-        url: queryURL,
-        type: 'GET',
-    }).done(function(response){
-        console.log(response); // logs object data info
+        // ajax call 
+        $.ajax({
+            url: queryURL,
+            type: 'GET',
+        }).done(function (response) {
+            console.log(response.data); // logs object data info
+            var results = response.data;
+            // loops through result gifs
+            for (var i = 0; i < results.length; i++) {
+                // creates div for result gifs
+                var gifDiv = $('<div class=gifs>');
+                // 
+                var movieImg = $("<img>");
+                movieImg.attr("src", results[i].images.fixed_height_still.url);
+                movieImg.attr("data-still", results[i].images.fixed_height_still.url);
+                movieImg.attr("data-animate", results[i].images.fixed_height.url);
+                movieImg.attr("data-state", "still");
+				movieImg.addClass('gif');
+
+                gifDiv.append(movieImg)
+
+                $("#gifs").prepend(gifDiv);
+
+                
+            }
 
 
-    })
+        })
     });
 
 
-
 });
-
-function searchGif(gifName) {
-    
-}
-
